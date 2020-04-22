97a62912b4fe98cd49f39f0f0255c959c725403b
Quoc Anh <quoc.anh91@gmail.com>
2020-04-22 23:30:21 +0700

    hehe

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use DB;
+use App\Post;
+
+class PostsController extends Controller
+{
+    public function show($slug)
+    {
+        // $post = DB::table('posts')->where('slug', $slug)->first();
+        $post = Post::where('slug', $slug)->firstOrFail();
+
+        return view('post', [
+            'post' => $post
+        ]);
+    }
+}
